e1fd975047310b65c5431f713193d523c0a2a4a6
Faaiz Mahmood <faizzafar44@gmail.com>
2025-07-15 17:07:55 +0500

    Implement form submission and confirmation page

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -42,11 +42,13 @@ function Reservation() {
     <main className='reservation-container'>
       <h1 style={{marginTop:'300px'}}>Book Now</h1>
       <ReservationProvider>
+
         <ReservationForm
           availableTimes={availableTimes}
           dispatchOnDateChange={dispatch}
           submitData={submitData}
         />
+        
       </ReservationProvider>
     </main>
   );
